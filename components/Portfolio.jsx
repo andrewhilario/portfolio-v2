42a9e7383bd7a6e98e91e350a265f676314149da
Drew Hilario <hilarioandrew12@gmail.com>
2023-04-23 10:58:38 +0800

    Added some projects and remove unamartshoppingmall from the projects

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { FaReact } from 'react-icons/fa';
-import { TbApi } from 'react-icons/tb';
+import { TbApi, TbBrandFirebase, TbBrandVite } from 'react-icons/tb';
 import {
   SiTailwindcss,
   SiDjango,
@@ -16,7 +16,7 @@ import {
   SiHtml5,
   SiCss3,
   SiJquery,
-  SiChakraui,
+  SiChakraui
 } from 'react-icons/si';
 import { TbBrandNextjs, TbBrandMongodb, TbBrandPrisma } from 'react-icons/tb';
 import { motion, AnimatePresence } from 'framer-motion';
@@ -32,7 +32,7 @@ const Portfolio = () => {
         </h1>
       </div>
       <div className='w-full flex flex-col md:px-10 gap-5 md:gap-[5rem] py-10'>
-        {/* FoxyAnime Web */}
+        {/* Kawaiifu Web */}
         <div className='w-full flex flex-col md:flex-row gap-4 md:gap-[2.5rem] md:justify-between'>
           <div className='w-full block md:w-1/2 h-[200px] md:h-[350px] lg:h-[450px] rounded-lg shadow-lg transition ease-in duration-100 hover:border-2 hover:border-slate-700'>
             <Image
@@ -129,11 +129,11 @@ const Portfolio = () => {
             </div>
           </div>
         </div>
-        {/* Unamart E-commerce */}
+        {/* Panda Pages */}
         <div className='w-full flex flex-col md:flex-row gap-4 md:gap-[2.5rem] md:justify-between'>
           <div className='block md:w-1/2 h-[200px] md:h-[350px] lg:h-[450px] rounded-lg shadow-lg transition ease-in duration-100 hover:border-2 hover:border-slate-700'>
             <Image
-              src='/assets/unamart.png'
+              src='/assets/pandapages.png'
               width={1920}
               height={1080}
               quality={100}
@@ -142,40 +142,37 @@ const Portfolio = () => {
           </div>
           <div className='md:w-1/2 flex flex-col gap-4 py-4'>
             <h1 className='font-extrabold text-3xl hover:text-blue-600 transition ease-in duration-100'>
-              Unamart E-Commerce
+              Panda Pages
             </h1>
             <p className='text-lg font-semibold'>
-              This is a Freelance project. I made this using Wordpress with
-              Woocommerce and Elementor. I also use Dokan for the Multi-Vendor
-              feature. It also has a custom theme. Unamart is a local store here
-              in the Philippines. They sell different kinds of products.I made
-              this for them so they can sell their products online.
+              Panda Pages is a Manga Reading Website that I made using Vite +
+              React JS. I also used Chakra UI for the styles. I use Consumet.org
+              API for the Manga data. It doesn't have authentication yet but I
+              will add it in the future. I also made a repo for this project.
+              You can check it out.
             </p>
             <div className='flex flex-wrap md:flex-nowrap items-center w-full md:w-3/4 gap-5'>
-              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-[#00749C] ease-in duration-150 hover:text-white'>
-                <p>Wordpress</p>
-                <SiWordpress className='text-4xl' />
+              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-purple-600 ease-in duration-150 hover:text-white'>
+                <p>Vite</p>
+                <TbBrandVite className='text-4xl' />
               </div>
-              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-[#8993be] ease-in duration-150 hover:text-white'>
-                <p>PHP</p>
-                <SiPhp className='text-4xl' />
+
+              <div className=' flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 rounded-lg cursor-pointer hover:bg-[#4FD1C5] text-slate-900 ease-in duration-150 hover:text-white'>
+                <p>Chakra UI</p>
+                <SiChakraui className='text-4xl ' />
               </div>
-              <div className=' flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 rounded-lg cursor-pointer hover:bg-[#92003B] text-slate-900 ease-in duration-150 hover:text-white'>
-                <p>Elementor</p>
-                <SiElementor className='text-4xl' />
-              </div>
-              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-[#96588a] ease-in duration-150 hover:text-white'>
-                <p>WooCommerce</p>
-                <SiWoo className='text-4xl ' />
+              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-[#FC3D6A] ease-in duration-150 hover:text-white'>
+                <p>Consumet.org</p>
+                <TbApi className='text-4xl ' />
               </div>
             </div>
             <div className='flex items-center gap-4'>
-              <Link href='https://unamartshoppingmall.com/'>
+              <Link href='https://pandapagesmanga.web.app/'>
                 <p className='px-6 py-2 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-700'>
                   Live
                 </p>
               </Link>
-              <Link href='https://github.com/andrewhilario/unamartshoppingmall'>
+              <Link href='https://github.com/andrewhilario/pandapages'>
                 <p className='px-6 py-2 bg-gray-500 text-white font-semibold rounded-lg hover:bg-gray-700'>
                   Repo
                 </p>
@@ -249,11 +246,11 @@ const Portfolio = () => {
             </div>
           </div>
         </div>
-        {/* Twitter Clone  */}
+        {/* Social Media App */}
         <div className='w-full flex flex-col md:flex-row gap-4 md:gap-[2.5rem] md:justify-between'>
           <div className='block md:w-1/2 h-[200px] md:h-[350px] lg:h-[450px] rounded-lg shadow-lg transition ease-in duration-100 hover:border-2 hover:border-slate-700'>
             <Image
-              src='/assets/twitterclone.png'
+              src='/assets/socialmediaappchakraui.png'
               width={1920}
               height={1080}
               quality={100}
@@ -262,39 +259,36 @@ const Portfolio = () => {
           </div>
           <div className='md:w-1/2 flex flex-col gap-4 py-4'>
             <h1 className='font-extrabold text-3xl hover:text-sky-500 transition ease-in duration-100'>
-              Twitter Clone
+              Socials App
             </h1>
             <p className='text-lg font-semibold'>
-              This is a personal project that I made using Next JS, MongoDB,
-              Prisma, and Tailwind CSS. It is a Twitter Clone that allows users
-              to post, like, and comment on posts. This project is not yet fully
+              This is a personal project that I made using Vite + React JS, with
+              Firebase for the backend and Database and also Chakra UI for the
+              components. It is a Social Media website that allows users to
+              post, like, and comment on posts. This project is not yet fully
               finished but I am planning to add more features in the future.
             </p>
             <div className='flex flex-wrap md:flex-nowrap items-center w-full md:w-3/4 gap-5'>
-              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-neutral-900 ease-in duration-150 hover:text-white'>
-                <p>Next.JS</p>
-                <TbBrandNextjs className='text-4xl' />
+              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-purple-600 ease-in duration-150 hover:text-white'>
+                <p>Vite</p>
+                <TbBrandVite className='text-4xl' />
               </div>
-              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-green-600 ease-in duration-150 hover:text-white'>
-                <p>MongoDB</p>
-                <TbBrandMongodb className='text-4xl' />
+              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-yellow-400 ease-in duration-150 hover:text-white'>
+                <p>Firebase</p>
+                <TbBrandFirebase className='text-4xl' />
               </div>
-              <div className=' flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 rounded-lg cursor-pointer hover:bg-[#38bdf8] text-slate-900 ease-in duration-150 hover:text-white'>
-                <p>Tailwind CSS</p>
-                <SiTailwindcss className='text-4xl ' />
-              </div>
-              <div className='flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 text-slate-900 rounded-lg cursor-pointer hover:bg-[#1a202c] ease-in duration-150 hover:text-white'>
-                <p>Prisma</p>
-                <TbBrandPrisma className='text-4xl ' />
+              <div className=' flex items-center font-bold text-md gap-2 p-2 border-2 border-slate-900 rounded-lg cursor-pointer hover:bg-[#4FD1C5] text-slate-900 ease-in duration-150 hover:text-white'>
+                <p>Chakra UI</p>
+                <SiChakraui className='text-4xl ' />
               </div>
             </div>
             <div className='flex items-center gap-4'>
-              <Link href='https://socialmedia-clone.vercel.app/'>
+              <Link href='https://socialmedia-chakraui.vercel.app/'>
                 <p className='px-6 py-2 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-700'>
                   Live
                 </p>
               </Link>
-              <Link href='https://github.com/andrewhilario/socialmedia-clone'>
+              <Link href='https://github.com/andrewhilario/socialmedia-chakraui'>
                 <p className='px-6 py-2 bg-gray-500 text-white font-semibold rounded-lg hover:bg-gray-700'>
                   Repo
                 </p>
